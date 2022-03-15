c02419fa20ade8be5972306d8fe274eeda5c1a4c
tangxiaofeng7 <45926593+tangxiaofeng7@users.noreply.github.com>
2022-03-15 17:46:15 +0800

    Delete web directory

--- web/src/pages/System/account/data.d.ts
+++
@@ -1,6 +0,0 @@
-export type CurrentUser = {
-  avatar?: string;
-  name?: string;
-  title?: string;
-  password?: string;
-};
--- web/src/pages/System/state/service.ts
+++
@@ -1,6 +0,0 @@
-import { request } from 'umi';
-import type { Systeminfo } from './data';
-
-export async function queryState(): Promise<{ data: Systeminfo }> {
-  return request('/serverInfo');
-}
